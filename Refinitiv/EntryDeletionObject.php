618884acdf943c3bf4f4c0b4ad93ffb5b8e7379c
Eldad Seter <eldad.seter@kaltura.com>
2020-07-20 17:07:12 +0300

    Entry Deletion fix - for future runs
    * fixed the deleteAllEntries function so that it does baseEntry.list with clientTag kmcng,  and thus supports lastPlayed
    * added the deleteEntriesFromInputFile function, which deletes entries from input file by entry id

--- Refinitiv/EntryDeletionObject.php
+++ Refinitiv/EntryDeletionObject.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('EntryAndFlavorActions.php');
+require_once('EntryAndFlavorActions.php');
 
 class EntryDeletionObject
 {
@@ -21,28 +21,27 @@ class EntryDeletionObject
 		$firstTry              = 1;
 		$message               = 'Total number of entries: ';
 		$trialsExceededMessage = 'Exceeded number of trials for this list. Moving on to next list' . "\n\n";
-		$mediaList = $this->entryAndFlavorActions->clientObject->doMediaList($mediaEntryFilter, $pager, $message, $trialsExceededMessage, $firstTry);
+		$baseEntryList         = $this->entryAndFlavorActions->clientObject->doBaseEntryList($mediaEntryFilter, $pager, $message, $trialsExceededMessage, $firstTry);
 
 		$i = 0;
-		while(count($mediaList->objects)) {
+		while(count($baseEntryList->objects)) {
 			echo 'Beginning of page: ' . ++$i . "\n";
-			echo 'Entries per page: ' . count($mediaList->objects) . "\n\n";
+			echo 'Entries per page: ' . count($baseEntryList->objects) . "\n\n";
 
 			/* @var $currentEntry KalturaMediaEntry */
-			foreach($mediaList->objects as $currentEntry) {
+			foreach($baseEntryList->objects as $currentEntry) {
 
 				$type = $this->entryAndFlavorActions->gettingTypeOfEntry($currentEntry);
 
 				$categoriesFullNameString = $this->entryAndFlavorActions->gettingCategoryNamesOfEntry($currentEntry);
 
-				//printing..
-				fputcsv($outputCsv, array($currentEntry->id, $currentEntry->name, $type, $categoriesFullNameString, $currentEntry->createdAt, $currentEntry->updatedAt, $currentEntry->lastPlayedAt));
+				//saving data to print..
+				$dataArray = array($currentEntry->id, $currentEntry->name, $type, $currentEntry->userId, $categoriesFullNameString, $currentEntry->createdAt, $currentEntry->lastPlayedAt);
 
 				//now we're ready to delete
-				$successMessage = 'Entry ' . $currentEntry->id . ' was deleted' . "\n";
+				$successMessage        = 'Entry ' . $currentEntry->id . ' was deleted' . "\n";
 				$trialsExceededMessage = 'Exceeded number of trials for this entry ' . $currentEntry->id . '. Moving on to next entry' . "\n\n";
-				$this->entryAndFlavorActions->clientObject->doMediaDelete($currentEntry->id, $successMessage, $trialsExceededMessage, $firstTry);
-
+				$this->entryAndFlavorActions->clientObject->doMediaDelete($currentEntry->id, $successMessage, $outputCsv, $dataArray, $trialsExceededMessage, $firstTry);
 			}
 
 			fputcsv($outputCsv, array('=====', '=====', '=====', '=====', '=====', '====='));
@@ -50,7 +49,7 @@ class EntryDeletionObject
 			//media . list - next iterations
 			$mediaEntryFilter->createdAtGreaterThanOrEqual = $currentEntry->createdAt + 1;
 			$trialsExceededMessage                         = 'Exceeded number of trials for this list. Moving on to next list' . "\n\n";
-			$mediaList                                     = $this->entryAndFlavorActions->clientObject->doMediaList($mediaEntryFilter, $pager, "", $trialsExceededMessage, $firstTry);
+			$baseEntryList                                 = $this->entryAndFlavorActions->clientObject->doBaseEntryList($mediaEntryFilter, $pager, "", $trialsExceededMessage, $firstTry);
 		}
 
 	}
@@ -66,15 +65,15 @@ class EntryDeletionObject
 		$firstTry              = 1;
 		$message               = 'Total number of entries: ';
 		$trialsExceededMessage = 'Exceeded number of trials for this list. Moving on to next list' . "\n\n";
-		$mediaList = $this->entryAndFlavorActions->clientObject->doMediaList($mediaEntryFilter, $pager, $message, $trialsExceededMessage, $firstTry);
+		$baseEntryList         = $this->entryAndFlavorActions->clientObject->doBaseEntryList($mediaEntryFilter, $pager, $message, $trialsExceededMessage, $firstTry);
 
 		$i = 0;
-		while(count($mediaList->objects)) {
+		while(count($baseEntryList->objects)) {
 			echo 'Beginning of page: ' . ++$i . "\n";
-			echo 'Entries per page: ' . count($mediaList->objects) . "\n\n";
+			echo 'Entries per page: ' . count($baseEntryList->objects) . "\n\n";
 
 			/* @var $currentEntry KalturaMediaEntry */
-			foreach($mediaList->objects as $currentEntry) {
+			foreach($baseEntryList->objects as $currentEntry) {
 
 				$type = $this->entryAndFlavorActions->gettingTypeOfEntry($currentEntry);
 
@@ -87,9 +86,62 @@ class EntryDeletionObject
 			//media . list - next iterations
 			$mediaEntryFilter->createdAtGreaterThanOrEqual = $currentEntry->createdAt + 1;
 			$trialsExceededMessage                         = 'Exceeded number of trials for this list. Moving on to next list' . "\n\n";
-			$mediaList                                     = $this->entryAndFlavorActions->clientObject->doMediaList($mediaEntryFilter, $pager, "", $trialsExceededMessage, $firstTry);
+			$baseEntryList                                 = $this->entryAndFlavorActions->clientObject->doBaseEntryList($mediaEntryFilter, $pager, "", $trialsExceededMessage, $firstTry);
 		}
+	}
 
+	public function deleteEntriesFromInputFile($inputEntryIdsFile, $outputPathCsv) {
+		$outputCsv = fopen($outputPathCsv, 'w');
+		fputcsv($outputCsv, array('EntryID', 'Name', 'MediaType', 'CategoriesFullName', 'FlavorNames', 'CreatedAt', 'UpdatedAt', 'LastPlayedAt'));
+
+		//0. get all entry ids from file
+		$entryIdsArray = $this->entryAndFlavorActions->getAllEntryIdsFromFile($inputEntryIdsFile);
+
+		//0a. do media list on those entries
+		$entryIds500       = array_slice($entryIdsArray, 0, 500);
+		$entryIds500String = implode(",", $entryIds500);
+
+		list($pager, $mediaEntryFilter) = $this->entryAndFlavorActions->definePagerAndFilterForInputFile($entryIds500String);
+
+		$firstTry              = 1;
+		$trialsExceededMessage = 'Exceeded number of trials for this list. Moving on to next list' . "\n\n";
+		$mediaList             = $this->entryAndFlavorActions->clientObject->doMediaList($mediaEntryFilter, $pager, "", $trialsExceededMessage, $firstTry);
+
+		$i = 0;
+		while(count($mediaList->objects)) {
+			echo "Beginning of page: " . ++$i . "\n";
+			echo "Count: " . count($mediaList->objects) . "\n\n";
+
+			/* @var $currentEntry KalturaMediaEntry */
+			foreach($mediaList->objects as $currentEntry) {
+
+				//1. get entry info and save it to print
+				$type = $this->entryAndFlavorActions->gettingTypeOfEntry($currentEntry);
+				$categoriesFullNameString = $this->entryAndFlavorActions->gettingCategoryNamesOfEntry($currentEntry);
+				$dataArray = array($currentEntry->id, $currentEntry->name, $type, $currentEntry->userId, $categoriesFullNameString, $currentEntry->createdAt, $currentEntry->lastPlayedAt);
+
+				//2. delete entry
+				$successMessage        = 'Entry ' . $currentEntry->id . ' was deleted' . "\n";
+				$trialsExceededMessage = 'Exceeded number of trials for this entry ' . $currentEntry->id . '. Moving on to next entry' . "\n\n";
+				$this->entryAndFlavorActions->clientObject->doMediaDelete($currentEntry->id, $successMessage, $outputCsv, $dataArray, $trialsExceededMessage, $firstTry);
+			}
+
+			echo "Last entry: " . $currentEntry->id . "\n";
+			echo "End of page\n\n";
+
+			//media . list - next page (iteration)
+			$entryIds500        = array_slice($entryIdsArray, $i * 500, 500);
+			$entryIds500String  = implode(",", $entryIds500);
+			if($entryIds500String == "") {
+				echo "End of entries" . "\n";
+				break;
+			}
+
+			$mediaEntryFilter->idIn = $entryIds500String;
+			$trialsExceededMessage = 'Exceeded number of trials for this list. Moving on to next list' . "\n\n";
+			$mediaList             = $this->entryAndFlavorActions->clientObject->doMediaList($mediaEntryFilter, $pager, "", $trialsExceededMessage, $firstTry);
+		}
+		echo "End of function\n";
 	}
 
 }
